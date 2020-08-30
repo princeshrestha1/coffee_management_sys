b240c9ea5f67879c03ad108e02eb6f93921b7c17
princeshrestha <princeshrestha@github.com>
2020-08-30 16:30:09 +0545

    ok

--- classes/database.py
+++ classes/database.py
@@ -1,4 +1,4 @@
-import pymysql
+'''import pymysql
 # Open database connection
 db = pymysql.connect("localhost","testuser","test123","coffeemanagementsystem" )
 # prepare a cursor object using cursor() method
@@ -18,3 +18,55 @@ print('mysql updated')
 db.rollback()
 # disconnect from server
 db.close()
+'''
+import pymysql
+
+class MyDb:
+    def __init__(self):
+        self.my_connection = pymysql.connect(
+                        host="localhost",
+                        user="root",
+                        password="",
+                        database="es19b"
+                        )
+        self.my_cursor = self.my_connection.cursor()
+
+    def iud(self, qry, values):
+        try:
+            self.my_cursor.execute(qry, values)
+            self.my_connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def insert_with_id_return(self, qry, values):
+        try:
+            self.my_cursor.execute(qry, values)
+            self.my_connection.commit()
+            return self.my_cursor.lastrowid
+        except Exception as e:
+            print(e)
+            return 0
+
+    def show_data(self, qry):
+        data = []
+        try:
+            self.my_cursor.execute(qry)
+            data = self.my_cursor.fetchall()
+            return data
+        except Exception as e:
+            print(e)
+            return data
+
+    def show_data_p(self, qry, values):
+        data = []
+        try:
+            self.my_cursor.execute(qry, values)
+            data = self.my_cursor.fetchall()
+            return data
+        except Exception as e:
+            print(e)
+            return data
+
+
---
+++ users/users.py
@@ -0,0 +1,23 @@
+from classes.connection import *
+
+class User:
+
+    def __init__(self, un, pw, t, n):
+
+        self.db = MyDb()
+
+    def register(self, un, pw, t, n):
+        qry = """INSERT INTO users (username, password, type, name)
+                VALUES (%s,%s,%s,%s)"""
+
+        values = (un, pw, t, n)
+        return self.db.iud()
+    def login(self, un, pw):
+        qry = """SELECT * FROM users WHERE username = %s AND password = %s"""
+        # values = (self.__username, self.__password)
+        values = (un, pw)
+        user = self.db.show_data_p(qry, values)
+        if len(user) > 0:
+            return True
+        else:
+            return False
